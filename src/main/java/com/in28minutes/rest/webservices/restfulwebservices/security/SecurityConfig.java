fd997177ba0855c20ec95f73d9de8fae66de2c3b
vp8791 <neetha.po@gmail.com>
2018-10-19 15:41:17 +0100

    Security Works

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/security/SecurityConfig.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/security/SecurityConfig.java
@@ -10,26 +10,32 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 //https://www.youtube.com/watch?v=4x-HgnJ31cg
 //https://www.boraji.com/spring-security-5-jdbc-based-authentication-example
+//http://www.springboottutorial.com/securing-rest-services-with-spring-boot-starter-security
+//C:\Users\Vanitha\Downloads\in28minutes.github.io-master\Website-SpringSecurityStarterWebApplication_Final (Delete at End)
+//admin /admin@123 : http://localhost:9051/
 
 //Password Generation
 //String encoded=new BCryptPasswordEncoder().encode("admin@123");
 // System.out.println(encoded);
+//Login as admin/admin@123
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private DataSource dataSource;
-
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
 		auth.jdbcAuthentication().dataSource(dataSource)
 				.usersByUsernameQuery("select USERNAME, PASSWORD, ENABLED" + " from LOGIN_USERS where USERNAME=?")
 				.authoritiesByUsernameQuery("select USERNAME, AUTHORITY " + "from AUTHORITIES where USERNAME=?")
-				.passwordEncoder(new BCryptPasswordEncoder());
+				.passwordEncoder(new BCryptPasswordEncoder()); 
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		
 		http.authorizeRequests().anyRequest().hasAnyRole("ADMIN", "USER").and().httpBasic(); // Authenticate users with
 																								// HTTP basic
 																								// authentication
